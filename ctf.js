9ad759dac2b63fbc35e333811b71146a207df85e
Carlos4683 <104417278+Carlos4683@users.noreply.github.com>
2022-04-26 09:05:23 +0200

    CTF -Sant Jordi

---
+++ ctf.js
@@ -0,0 +1,9 @@
+var resposta = document.getElementById('enigma').value;
+
+function respostaEngima(){
+    if(resposta=='Rosa'){
+        alert("Correcte!! flag{C@rl05R0dr1gu3z}");
+    }else{
+        alert("Correcte!! flag{C@rl05R0dr1gu3z}");
+    }
+}
